03ac422966ec4f417b7e6a4f88911a382d1acfe9
Travis Thomsen <rivethead42@gmail.com>
2026-06-27 17:45:44 -0500

    Add checkov exclusions

--- terraform/modules/ecr/main.tf
+++ terraform/modules/ecr/main.tf
@@ -1,11 +1,20 @@
 # ECR repository for payment-api images with immutable tags and lifecycle rules.
 
+data "aws_kms_alias" "ecr" {
+  name = "alias/aws/ecr"
+}
+
 resource "aws_ecr_repository" "this" {
   name                 = var.repository_name
   image_tag_mutability = "IMMUTABLE"
 
   image_scanning_configuration {
     scan_on_push = true
+  }
+
+  encryption_configuration {
+    encryption_type = "KMS"
+    kms_key         = data.aws_kms_alias.ecr.target_key_arn
   }
 
   tags = var.tags
